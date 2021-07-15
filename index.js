511172b4daf5d7bcea876017b8a20ac058e777f9
SOM Visai <visai.s@mangotracking.com>
2021-07-15 19:04:12 +0700

    Update - handleEvent return null - 20210715 - 1904

--- index.js
+++ index.js
@@ -65,7 +65,8 @@ function handleEvent(event) {
   const echo = { type: 'text', text: event.message.text };
 
   // use reply API
-  return client.replyMessage(event.replyToken, echo);
+  //return client.replyMessage(event.replyToken, echo);
+  return ; 
 }
 
 // listen on port
